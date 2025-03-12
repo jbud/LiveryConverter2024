2fab2da176dd1b153a6cdf44c46aff3c45b69e2d
jbud <jbudorg@gmail.com>
2025-03-12 05:19:30 -0700

    Refactor Process Handling.
    * MainThread no longer freezes during any task.
    * Tasks will spawn minimal windows, where possible no window will spawn and the debug output will show STDOUT from the task.
    * Refactor subroutines for readability
    * Add a neat lil progress ring doohicky that does the spin while tasks are running

---
+++ ExeClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveryConverter2024
+{
+    internal class ExeClass
+    {
+        public async Task SpawnProc(string proc, string args, MainWindow dbg)
+        {
+            using (Process? p2 = new Process())
+            {
+                p2.StartInfo.FileName = proc;
+                p2.StartInfo.Arguments = args;
+                p2.StartInfo.RedirectStandardOutput = true;
+                p2.StartInfo.CreateNoWindow = true;
+                p2.StartInfo.UseShellExecute = false;
+                p2.EnableRaisingEvents = true;
+                p2.OutputDataReceived += (object sender, DataReceivedEventArgs args) => 
+                {
+                    dbg.Dispatcher.Invoke(() =>
+                    {
+                        dbg.DebugConsole = " " + args.Data + "\n";
+                    });
+                    
+                };
+                p2.Start();
+                p2.BeginOutputReadLine();
+                await p2.WaitForExitAsync();
+            }
+        }
+        public async Task ProcMon(string processName, MainWindow dbg)
+        {
+            Thread.Sleep(2000);
+            Process[]? processes = Process.GetProcessesByName(processName);
+            foreach (Process p in processes)
+            {
+                dbg.Dispatcher.Invoke(() =>
+                {
+                    dbg.DebugConsole = "Found Process " + p.ProcessName + " Waiting for exit...\n";
+                });
+                await p.WaitForExitAsync();
+                
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+using Wpf.Ui.Controls;
 
 namespace LiveryConverter2024
 {
@@ -25,7 +26,7 @@ namespace LiveryConverter2024
         public MainWindow()
         {
             InitializeComponent();
-            prepare_Dirs();
+            Prepare_Dirs();
             projectFolder.Text = Properties.Settings.Default.projectPath;
             textureFolder.Text = Properties.Settings.Default.texturePath;
             sdkPath.Text = Properties.Settings.Default.sdkPath;
@@ -41,6 +42,15 @@ namespace LiveryConverter2024
                     break;
             }
         }
+
+        public string DebugConsole
+        {
+            get { return debug.Text; }
+            set { 
+                debug.AppendText(value);
+                debug.ScrollToEnd();
+            }
+        }
         private readonly string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "com.budzique.livery-converter.app\\");
         private Process p1;
         private Process p2;
@@ -49,18 +59,18 @@ namespace LiveryConverter2024
         {
             foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
             foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "PackageSources\\SimObjects\\Airplanes\\livery-converter-2024\\common\\texture\\"));
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "PackageDefinitions\\"));
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "DDSINPUT\\"));
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "TEMP\\"));
         }
 
-        private void prepare_Dirs()
+        private void Prepare_Dirs()
         {
             debug.AppendText("Creating Directories\n");
             debug.ScrollToEnd();
             System.IO.Directory.CreateDirectory(path);
             clearPath(new DirectoryInfo(path));
-            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "PackageSources\\SimObjects\\Airplanes\\livery-converter-2024\\common\\texture\\"));
-            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "PackageDefinitions\\"));
-            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "DDSINPUT\\"));
-            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, "TEMP\\"));
             Assembly asm = Assembly.GetExecutingAssembly();
             string[] resources = asm.GetManifestResourceNames();
 
@@ -163,6 +173,99 @@ namespace LiveryConverter2024
         }
         private void generateKTX2()
         {
+            
+
+            string stearing = " ";
+            if (Properties.Settings.Default.store == "Steam")
+            {
+                stearing = " -forceSteam ";
+            }
+            using (p3 = new Process()) 
+            {
+                p3.StartInfo.FileName = Properties.Settings.Default.sdkPath + "\\tools\\bin\\fspackagetool.exe";
+                p3.StartInfo.Arguments = "-nopause"+stearing+ path + "livery-converter-2024.xml";
+                p3.StartInfo.RedirectStandardOutput = true;
+                p3.Start();
+                string output = p3.StandardOutput.ReadToEnd();
+                p3.WaitForExit();
+                debug.AppendText(output);
+                debug.ScrollToEnd();
+            }
+            
+            /**
+             * TODO:
+             *  move new textures to desired location: Properties.Settings.Default.texturePath
+             *  run MSFSLayoutGenerator.exe
+             * 
+             * **/
+        }
+
+        private void generateXMLs()
+        {
+            DirectoryInfo d = new DirectoryInfo(path + "TEMP");
+            FileInfo[] i = d.GetFiles();
+            Dispatcher.Invoke(() =>
+            {
+                DebugConsole = "Sorting Textures... Generating XMLs...\n";
+            });
+            string pkgSourceDir = path + "PackageSources\\SimObjects\\Airplanes\\livery-converter-2024\\common\\texture\\";
+
+            foreach (FileInfo f in i)
+            {
+                string shortFileName = f.Name;
+                string shortNewName = shortFileName.Substring(0, shortFileName.Length - 4);
+                string ext = f.Extension;
+                string file = f.FullName;
+                //File.Move(file, newFile);
+                if (file.Contains("ALBD"))
+                {
+                    XDocument xmldoc = new XDocument(
+                        new XElement("BitmapConfiguration",
+                            new XElement("BitmapSlot", "MTL_BITMAP_DECAL0"),
+                            new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
+                        )
+                    );
+                    xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
+                    File.Move(file, pkgSourceDir + shortNewName);
+                }
+                else
+                if (file.Contains("COMP"))
+                {
+                    XDocument xmldoc = new XDocument(
+                        new XElement("BitmapConfiguration",
+                            new XElement("BitmapSlot", "MTL_BITMAP_METAL_ROUGH_AO"),
+                            new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH"),
+                            new XElement("ForceNoAlpha", true)
+                        )
+                    );
+                    xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
+                    File.Move(file, pkgSourceDir + shortNewName);
+                }
+                else
+                if (file.Contains("DECAL"))
+                {
+                    XDocument xmldoc = new XDocument(
+                        new XElement("BitmapConfiguration",
+                            new XElement("BitmapSlot", "MTL_BITMAP_DECAL0"),
+                            new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
+                        )
+                    );
+                    xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
+                    File.Move(file, pkgSourceDir + shortNewName);
+                }
+                else
+                if (file.Contains("NORM"))
+                {
+                    XDocument xmldoc = new XDocument(
+                        new XElement("BitmapConfiguration",
+                            new XElement("BitmapSlot", "MTL_BITMAP_NORMAL"),
+                            new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
+                        )
+                    );
+                    xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
+                    File.Move(file, pkgSourceDir + shortNewName);
+                }
+            }
             XDocument xPkgDef = new XDocument(
                 new XElement("AssetPackage", new XAttribute("Version", "0.1.0"),
                     new XElement("ItemSettings",
@@ -196,44 +299,52 @@ namespace LiveryConverter2024
                 new XAttribute("MetadataFolderName", "PackagesMetadata")
             };
             XDocument xProject = new XDocument(
-                new XElement("Project", projAttrs, 
+                new XElement("Project", projAttrs,
                     new XElement("OutputDirectory", "."),
                     new XElement("TemporaryOutputDirectory", "_PackageInt"),
-                    new XElement("Packages", 
+                    new XElement("Packages",
                         new XElement("Package", "PackageDefinitions\\livery-converter-2024.xml")
                     )
                 )
             );
             xProject.Save(path + "livery-converter-2024.xml");
+        }
 
+        private async void ProcessHandler()
+        {
+            this.Dispatcher.Invoke((Action)(() => {
+                Progress.Visibility = Visibility.Visible;
+                Progress.IsIndeterminate = true;
+            }));
+            ExeClass exeClass = new();
+            await exeClass.SpawnProc(path + "texconv.exe", "-r:keep " + path + "DDSINPUT\\*.png.dds -o " + path + "TEMP -f:rgba -ft:png", this);
+            await exeClass.SpawnProc(path + "texconv.exe", "-r:keep " + path + "DDSINPUT\\*.tif.dds -o " + path + "TEMP -f:rgba -ft:tif", this);
+            generateXMLs();
             string stearing = " ";
             if (Properties.Settings.Default.store == "Steam")
             {
                 stearing = " -forceSteam ";
             }
-            using (p3 = new Process()) 
+            Dispatcher.Invoke(() =>
             {
-                p3.StartInfo.FileName = Properties.Settings.Default.sdkPath + "\\tools\\bin\\fspackagetool.exe";
-                p3.StartInfo.Arguments = "-nopause"+stearing+ "-outputtoseparateconsole " + path + "livery-converter-2024.xml";
-                p3.StartInfo.RedirectStandardOutput = true;
-                p3.Start();
-                string output = p3.StandardOutput.ReadToEnd();
-                p3.WaitForExit();
-                debug.AppendText(output);
-                debug.ScrollToEnd();
-            }
-            
-            /**
-             * TODO:
-             *  finish package folder generation and move textures. (_PackageInt, PackagesMetadata, Packages) ?
-             *  spawn fspackagetool.exe from sdk and monitor for completion (mon proc FlightSimulator2024.exe?)
-             *  move new textures to desired location: Properties.Settings.Default.texturePath
-             *  run MSFSLayoutGenerator.exe
-             * 
-             * **/
+                DebugConsole = "Spawning MSFS2024 Package Manager, Please Wait...\n";
+            });
+
+            await exeClass.SpawnProc(Properties.Settings.Default.sdkPath + "\\tools\\bin\\fspackagetool.exe", "-nopause -outputtoseparateconsole " + stearing + path + "livery-converter-2024.xml", this);
+            await exeClass.ProcMon("FlightSimulator2024", this);
+            this.Dispatcher.Invoke((Action)(() => {
+                Progress.Visibility = Visibility.Hidden;
+            }));
+            Dispatcher.Invoke(() =>
+            {
+                DebugConsole = "Conversion Complete, tune in next week for the rest of the functionality!\n";
+            });
         }
+
+
         private void button4_Click(object sender, RoutedEventArgs e)
         {
+            //Prepare_Dirs();
             var fileDialog = new OpenFileDialog
             {
                 Filter = "Direct Draw Surface files (*.DDS)|*.DDS|" + "All files (*.*)|*.*",
@@ -241,6 +352,8 @@ namespace LiveryConverter2024
             };
             if (fileDialog.ShowDialog() == true)
             {
+                
+                
                 foreach (string f in fileDialog.FileNames)
                 {
                     debug.AppendText(f + "\n");
@@ -248,95 +361,7 @@ namespace LiveryConverter2024
                     File.Copy(f, path + "DDSINPUT\\" + System.IO.Path.GetFileName(f));
 
                 }
-                using (p2 = new Process())
-                {
-                    p2.StartInfo.FileName = path + "texconv.exe";
-                    p2.StartInfo.Arguments = "-r:keep " + path + "DDSINPUT\\*.tif.dds -o " + path + "TEMP -f:rgba -ft:tif";
-                    p2.StartInfo.RedirectStandardOutput = true;
-                    p2.StartInfo.CreateNoWindow = true;
-                    p2.Start();
-                    string output = p2.StandardOutput.ReadToEnd();
-                    p2.WaitForExit();
-                    debug.AppendText(output);
-                    debug.ScrollToEnd();
-                }
-                using (p1 = new Process())
-                {
-                    p1.StartInfo.FileName = path + "texconv.exe"; 
-                    p1.StartInfo.Arguments = "-r:keep " + path + "DDSINPUT\\*.png.dds -o " + path + "TEMP -f:rgba -ft:png"; //
-                    p1.StartInfo.RedirectStandardOutput = true;
-                    p1.EnableRaisingEvents = true;
-                    p1.Exited += new EventHandler(p1_Exited);
-                    p1.Start();
-                    string output = p1.StandardOutput.ReadToEnd();
-                    p1.WaitForExit();
-                    debug.AppendText(output);
-                    debug.ScrollToEnd();
-                }
-                DirectoryInfo d = new DirectoryInfo(path + "TEMP");
-                FileInfo[] i = d.GetFiles();
-                debug.AppendText("Sorting textures\n");
-                debug.ScrollToEnd();
-
-                string pkgSourceDir = path + "PackageSources\\SimObjects\\Airplanes\\livery-converter-2024\\common\\texture\\";
-
-                foreach (FileInfo f in i)
-                {
-                    string shortFileName = f.Name;
-                    string shortNewName = shortFileName.Substring(0, shortFileName.Length - 4);
-                    string ext = f.Extension;
-                    string file = f.FullName;
-                    //File.Move(file, newFile);
-                    if (file.Contains("ALBD"))
-                    {
-                        XDocument xmldoc = new XDocument(
-                            new XElement("BitmapConfiguration",
-                                new XElement("BitmapSlot", "MTL_BITMAP_DECAL0"),
-                                new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
-                            )
-                        );
-                        xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
-                        File.Move(file, pkgSourceDir + shortNewName);
-                    }
-                    else
-                    if (file.Contains("COMP"))
-                    {
-                        XDocument xmldoc = new XDocument(
-                            new XElement("BitmapConfiguration",
-                                new XElement("BitmapSlot", "MTL_BITMAP_METAL_ROUGH_AO"),
-                                new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH"),
-                                new XElement("ForceNoAlpha", true)
-                            )
-                        );
-                        xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
-                        File.Move(file, pkgSourceDir + shortNewName);
-                    }
-                    else
-                    if (file.Contains("DECAL"))
-                    {
-                        XDocument xmldoc = new XDocument(
-                            new XElement("BitmapConfiguration",
-                                new XElement("BitmapSlot", "MTL_BITMAP_DECAL0"),
-                                new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
-                            )
-                        );
-                        xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
-                        File.Move(file, pkgSourceDir + shortNewName);
-                    }
-                    else
-                    if (file.Contains("NORM"))
-                    {
-                        XDocument xmldoc = new XDocument(
-                            new XElement("BitmapConfiguration",
-                                new XElement("BitmapSlot", "MTL_BITMAP_NORMAL"),
-                                new XElement("UserFlags", new XAttribute("type", "_DEFAULT"), "QUALITYHIGH")
-                            )
-                        );
-                        xmldoc.Save(pkgSourceDir + shortNewName + ".xml");
-                        File.Move(file, pkgSourceDir + shortNewName);
-                    }
-                }
-                generateKTX2();
+                ProcessHandler();
             }
 
         }
